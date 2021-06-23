835659e0d763a5b38eb9c25ac1f7d52f6fbb80ad
Henrique Luhm <henriqueluhmwork@gmail.com>
2021-06-23 17:17:51 -0300

    added auth context, home and new room screens.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Button } from "./components/Button";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <Button text="Botão 1" />
+    <AuthContextProvider>
+      <BrowserRouter>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewRoom}/>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
