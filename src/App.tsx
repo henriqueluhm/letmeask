4e7424ff4ca4b73efeeb2c9c3c594de82e73960f
Henrique Luhm <henriqueluhmwork@gmail.com>
2021-06-24 01:01:42 -0300

    added first viewing of the room screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
+import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -9,8 +10,11 @@ function App() {
   return (
     <AuthContextProvider>
       <BrowserRouter>
+        <Switch>
           <Route path="/" exact component={Home}/>
-          <Route path="/rooms/new" component={NewRoom}/>
+          <Route path="/rooms/new"  component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </BrowserRouter>
     </AuthContextProvider>
   );
